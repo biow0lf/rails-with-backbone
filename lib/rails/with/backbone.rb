43e4b898a004c5dc88fb715f411c4a07b37a7126
Igor Zubkov <igor.zubkov@gmail.com>
2017-08-22 22:12:05 +0300

    Init

---
+++ lib/rails/with/backbone.rb
@@ -0,0 +1,9 @@
+require "rails/with/backbone/version"
+
+module Rails
+  module With
+    module Backbone
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/rails/with/backbone/version.rb
@@ -0,0 +1,7 @@
+module Rails
+  module With
+    module Backbone
+      VERSION = "0.1.0"
+    end
+  end
+end
